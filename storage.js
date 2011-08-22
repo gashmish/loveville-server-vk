1db0f3225accdc0de567b531e870263d06db45f7
mad <owner.mad.epa@gmail.com>
2011-08-22 07:06:48 +0400

    Implement simple vk storage access

--- storage.js
+++ storage.js
@@ -1,23 +1,60 @@
+var https = require('https');
 
-Storage = function (room_id) {
-    this.id = room_id;
-    this.MAX_SIDE_PLAYERS = 12;
-    this.PLAY_TIME = 60000;
-    this.boys = [];
-    this.girls = [];
+require('./storage-config.js');
+
+charts_key = 1000;
+stats_key = 1001;
+
+vk_api_url = 'api.vkontakte.ru';
+
+Storage = function () {    
 };
 
 Storage.prototype = {
-  constructor: Storage,  
-  
-  get_charts : function (user_id, callback) {
-    var charts = {"chargs" : "test" };
-    callback(charts);
-  },
-  
-  get_user_stats : function (user_id, callback) {
-    var stats = {"stats" : "test" };
-    callback(stats);
-  }
-  
+    constructor: Storage,
+    get_charts: function(user_id, callback) {
+	var stats = {"stats" : "test" };
+	callback(stats);
+
+	// this.get_data(charts_key, user_id, callback);
+    },
+    get_user_stats: function(user_id, callback) {
+	var charts = {"chargs" : "test" };
+	callback(charts);
+	
+        // this.get_data(stats_key, user_id, callback);
+    },
+    get_data: function(key, id, cb) {
+        https.get({
+            host: vk_api_url,
+            path: '/method/storage.get?key=' + key + '&id=' + id + '&access_token=' + access_token
+        }, function(res) {
+            res.setEncoding('ascii');
+            res.on('data', function (d) { 
+                       var data = JSON.parse(d);
+
+		       console.log(data);
+
+                       try {
+                           data = JSON.parse(data.response);
+                       } catch (err) { console.log(err); data = ""; }
+
+                       cb(data);
+                   });
+        }).on('error', function(e) {
+            console.error(e);
+        });
+    },
+    set_data: function(key, value, id, cb) {
+        https.get({
+            host: vk_api_url,
+            path: '/method/storage.get?key=' + key + '&id=' + id + '&value=' + value + '&access_token=' + access_token
+        }, function(res) {
+            res.setEncoding('ascii');
+
+            // res.on('data', cb);
+        }).on('error', function(e) {
+            console.error(e);
+        });
+    }
 };
